b69c493474c71a2dd2cc05e2ba1ce294175a6db5
Meysam Rezazadeh <mey.rezazadeh@gmail.com>
2022-11-18 13:02:57 +0330

    Update

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while True:
-    RPC.update(
-        large_image="pepejam",
-        large_text="AliDice Song",
-        details="Thinking...",
-        state="AliDice Song",
-        start=start,
-        buttons=[
-            {"label": "StepBros", "url": "https://discord.gg/H6Dn3ZEhXH"},
-            {"label": "Song", "url": "https://www.youtube.com/watch?v=xfr64zoBTAQ"}
-        ]
-    )
-    time.sleep(60)
+	RPC.update(
+		large_image="pepejam",
+		large_text="AliDice Song",
+		details="Thinking...",
+		state="AliDice Song",
+		start=start,
+		buttons=[
+			{"label": "StepBros", "url": "https://discord.gg/H6Dn3ZEhXH"},
+			{"label": "Song", "url": "https://www.youtube.com/watch?v=xfr64zoBTAQ"}
+		]
+	)
+	time.sleep(60)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Discord-Custom-Status
+Show a custom discord status on your discord profile
+
+## Setup
+Run this followed commands:
+
+``` bash
+# Install dependencies (only the first time)
+pip3 install -r requirements.txt
+
+# Run the app
+python3 main.py
+```
